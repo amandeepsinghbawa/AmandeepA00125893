337a39d8dfa998a7f274b34ecff2cfdf6fff0342
amandeep <aman_deep2020@yahoo.com>
2021-09-22 15:17:35 -0400

    Aman_A00125893.2

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -12,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
      //My Name is Amandeep Singh//
+
+        //Welcome to Androids Development world//
+        //Canadore College//
     }
 }
